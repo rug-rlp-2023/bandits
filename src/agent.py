e66538226baca98fa528ee3ca605fe1502171547
MordoTheHacker <91093022+MordoTheHacker@users.noreply.github.com>
2023-11-30 16:11:16 +0100

    Benchmarking learning parameters landmark
    Benchmarking setup in code for variable improvment

--- src/agent.py
+++ src/agent.py
@@ -1,38 +1,81 @@
 import numpy as np
+import math
 
 class Agent:
     def __init__(self, strategy):
         # default strategy is greedy
         self.strategy = strategy
         self.cumilative_reward= 0
+        # Strategy Variables
+        self.temperature = 0.5
+        self.temperature_action = 2
+        self.epsilon = 0.3
+        self.timesteps = 0
+        self.c = 1 
+        self.a_prefence_rate = 1
         
     def set_bandit(self, new_bandit):
-        self.initial_value = 0
+        if self.strategy == "optimistic greedy":
+            self.initial_value = 1
+        else:
+            self.initial_value = 0
         self.bandit = new_bandit
+        # Action Preferences
+        self.action_preferences = np.zeros(self.bandit.nr_arms, dtype = float)
+        # Q values
         self.q_values = np.full(self.bandit.nr_arms, self.initial_value, dtype = float)
         self.arm_pulls = np.zeros(self.bandit.nr_arms)
 
-    def choose_arm(self, epsilon):
-        # GREEDY
-        if self.strategy == "greedy":
+    def choose_arm(self):
+        self.timesteps += 1
+        # ACTION PREFERENCES
+        if self.strategy == "action preference":
+            exponentiated_values = np.exp(self.action_preferences / self.temperature)
+            probabilities = exponentiated_values / np.sum(exponentiated_values)
+            chosen_arm = np.random.choice(len(self.action_preferences), p=probabilities)
+        # SOFTMAX
+        elif self.strategy == "softmax":
+            exponentiated_values = np.exp(self.q_values / self.temperature)
+            probabilities = exponentiated_values / np.sum(exponentiated_values)
+            chosen_arm = np.random.choice(len(self.q_values), p=probabilities)
+        # UCB
+        elif self.strategy == "UCB":
+            ucb = []
+            for arm in range(self.bandit.nr_arms):
+                if self.arm_pulls[arm] == 0:
+                    ucb.append(9999) #very large value for unexplored arms
+                else: 
+                    ucb.append(self.q_values[arm] + self.c * np.sqrt(np.log(self.timesteps) / self.arm_pulls[arm]))
+            chosen_arm = np.argmax(ucb)
+        else: 
+            # GREEDY
             max_indices = np.where(self.q_values == np.max(self.q_values))[0]   # Choose the arm with the highest estimated value (Q-value)
             chosen_arm = np.random.choice(max_indices)   # Randomly choose one of the indices
-        # EPSILON GREEDY
-        elif self.strategy == "epsilon greedy":
-            if np.random.rand() < epsilon: # epsilon value
+            # EPSILON GREEDY
+            if self.strategy == "epsilon greedy" and np.random.rand() < self.epsilon: 
                 chosen_arm = np.random.choice(self.bandit.nr_arms)
-            else:
-                max_indices = np.where(self.q_values == np.max(self.q_values))[0]  
-                chosen_arm = np.random.choice(max_indices)   
+                    
         reward = self.bandit.pull_arm(chosen_arm)
         self.__update_qvalues(chosen_arm, reward)
     
     def __update_qvalues(self, chosen_arm, reward):
         self.cumilative_reward += reward
         self.arm_pulls[chosen_arm] += 1
-        #print(self.q_values)
-        #print(self.q_values[chosen_arm])
-        #temp = self.q_values[chosen_arm]
-        self.q_values[chosen_arm] = self.q_values[chosen_arm] + float(((reward - self.q_values[chosen_arm]) / self.arm_pulls[chosen_arm]))
-        #print(self.q_values[chosen_arm],"=",temp,"+","((",reward,"-",temp,")/",self.arm_pulls[chosen_arm],")")
-        #print(self.q_values)
+
+        # Updates action preferences
+        if self.strategy == "action preference":
+            # calculates pi_t(a) for all arms
+            exp_preferences = np.exp(self.action_preferences)
+            pi_t = exp_preferences / np.sum(exp_preferences)
+
+            for preference in range(self.bandit.nr_arms):
+                average_reward = self.cumilative_reward / self.timesteps
+                if chosen_arm == preference:
+                    self.action_preferences[preference] += self.a_prefence_rate * (reward - average_reward) * (1 - pi_t[preference])
+                else:
+                    self.action_preferences[preference] -= self.a_prefence_rate * (reward - average_reward) * pi_t[preference]
+        # Updates q_values
+        else:
+            self.q_values[chosen_arm] = self.q_values[chosen_arm] + float(((reward - self.q_values[chosen_arm]) / self.arm_pulls[chosen_arm]))
+        
+        
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from agent import Agent
 if __name__ == "__main__":
     # Initialize experiment hyperparameters
     n = 100 # Number of scenarios
-    k = 10 # Number of different arms (choices)
+    k = 5 # Number of different arms (choices)
 
     # Create bandits
     gaussian_bandits = []
@@ -19,40 +19,88 @@ if __name__ == "__main__":
         bernoulli_bandits.append(bernoulli_bandit)
 
     # Create agents
-    strategies = ["epsilon greedy"]#, "greedy", "optimistic greedy", "softmax", "upper-confidence bound", "action preference"]
+    strategies = ["action preference"]#["greedy", "epsilon greedy", "optimistic greedy", "softmax", "UCB", "action preference"]
 
     # Start experiments
-    t = 100 # Number of tries for each bandit
+    t = 500 # Number of tries for each bandit
+    
+    best_pulls_gaussian = 0
+    gaussian_assist = 0
+    best_pulls_bernoulli = 0
+    bernoulli_assist = 0
+    best_param_gaus = -1
+    best_param_bern = -1
 
-    correct_pulls = []
-    epsilon_pulls = []
-    epsilons = []
-    epsilon = 0.01
-    while epsilon < 1:
+    temp = 2
+    while temp > 0:
         for strategy in strategies:
+            average_gaussian_reward = 0
+            average_bernoulli_reward = 0
+            correct_pulls_gaussian = 0
+            correct_pulls_bernoulli = 0
+
             for bandit in gaussian_bandits:
                 #make agent with strategy
                 agent = Agent(strategy)
                 agent.set_bandit(bandit)
+                agent.temperature_action = temp
                 # For T choose an arm based on agent strategy
                 for iteration in range(t):
-                    agent.choose_arm(epsilon)
+                    agent.choose_arm()
                 """
-                print("---NEXT AGENT---")
+                print("---NEXT AGENT GAUSSIAN---")
                 print("strategy: ", agent.strategy)
                 print("total reward: ", agent.cumilative_reward)
                 print("correct pulls:", agent.arm_pulls[np.argmax(bandit.means)])
                 print("arm pull array: ", agent.arm_pulls)
                 print("q_value array: ", agent.q_values)
+                print("action preferences:", agent.action_preferences)
                 print("bandit arm means: ", bandit.means)
                 """
-                epsilon_pulls.append(agent.arm_pulls[np.argmax(bandit.means)])  
-        correct_pulls.append(sum(epsilon_pulls)/len(epsilon_pulls)) 
-        epsilons.append(epsilon)  
-        epsilon += 0.01
-    print(correct_pulls, epsilons)
-    print("Best epsilon:", correct_pulls[np.argmax(correct_pulls)], epsilons[np.argmax(correct_pulls)])
+                average_gaussian_reward += agent.cumilative_reward
+                correct_pulls_gaussian += agent.arm_pulls[np.argmax(bandit.means)]
+            for bandit in bernoulli_bandits:
+                #make agent with strategy
+                agent = Agent(strategy)
+                agent.set_bandit(bandit)
+                # For T choose an arm based on agent strategy
+                for iteration in range(t):
+                    agent.choose_arm()
+                """
+                print("---NEXT AGENT BERNOULLI---")
+                print("strategy: ", agent.strategy)
+                print("total reward: ", agent.cumilative_reward)
+                print("correct pulls:", agent.arm_pulls[np.argmax(bandit.probabilities)])
+                print("arm pull array: ", agent.arm_pulls)
+                print("q_value array: ", agent.q_values)
+                print("action preferences:", agent.action_preferences)
+                print("bandit arm means: ", bandit.probabilities)
+                """
+                average_bernoulli_reward += agent.cumilative_reward
+                correct_pulls_bernoulli += agent.arm_pulls[np.argmax(bandit.probabilities)]
+            """
+            print(strategy, "agent: ")
+            print("\tAVG reward gaussian =", average_gaussian_reward / n)
+            print("\tCorrect pulls gaussian =", correct_pulls_gaussian * 100 / t / n, "%")
+            print("\tAVG reward bernoulli =", average_bernoulli_reward / n)
+            print("\tCorrect pulls bernoulli =", correct_pulls_bernoulli * 100 / t / n, "%")
+            """
             
+            if best_pulls_gaussian < correct_pulls_gaussian * 100 / t / n:
+                best_pulls_gaussian = correct_pulls_gaussian * 100 / t / n
+                bernoulli_assist = correct_pulls_bernoulli * 100 / t / n
+                best_param_gaus = temp
+            if best_pulls_bernoulli < correct_pulls_bernoulli * 100 / t / n:
+                best_pulls_bernoulli = correct_pulls_bernoulli * 100 / t / n
+                gaussian_assist = correct_pulls_gaussian * 100 / t / n
+                best_param_bern = temp
+        
+        temp -= 0.1
+    print("Gaus:",best_pulls_gaussian, gaussian_assist, "\nBern:", best_pulls_bernoulli, bernoulli_assist, "\nParam val:", best_param_gaus, best_param_bern)
+
+
+        
+
                 
                 
 
